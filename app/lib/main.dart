19cabe6ea5ec80126ed67c04c75ba41e6265cb4e
Robert <Robcio255@Onet.pl>
2023-09-12 16:49:03 +0200

    Updated project

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,20 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("KOCI KOCHAM")),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isClicked = false;
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Przykład Fluttera"),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: _isLoading
+                  ? CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation(Colors.white),
+                    )
+                  : Text("Kliknij mnie"),
+              onPressed: () {
+                if (!_isLoading) {
+                  setState(() {
+                    _isLoading = true;
+                  });
+
+                  Future.delayed(Duration(seconds: 3), () {
+                    setState(() {
+                      _isClicked = true;
+                      _isLoading = false;
+                    });
+                  });
+                }
+              },
+            ),
+            SizedBox(width: 10),
+            Text(_isClicked ? "Kliknięto!" : "Nie kliknięto"),
+          ],
         ),
-        body: Center(child: Text("Poka stopy")),
       ),
     );
   }
